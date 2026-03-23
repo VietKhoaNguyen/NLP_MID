30d3aad8a8b20c8640d3cd7ee504b243698e9785
NVk925 <khoanv.23bi14223@usth.edu.vn>
2026-03-23 20:19:38 +0700

    Delete scripts/test_svm.py

--- scripts/test_svm.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-
-from utils.predict import predict_svm
-
-text = "Quán ăn rất ngon và phục vụ tốt"
-
-print("Prediction:", predict_svm(text))
